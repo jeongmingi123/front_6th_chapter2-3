57b93aa4d22cae367e85491ca48e7810415de35a
jeongmingi123 <60804074+jeongmingi123@users.noreply.github.com>
2025-08-14 01:45:32 +0900

    feat : post ui import index 생성

---
+++ src/features/post/ui/index.ts
@@ -0,0 +1,3 @@
+export { OpenDetailPost } from "../../open-detail-post/ui/OpenDetailPost"
+export { OpenEditPost } from "../../open-edit-post/ui/OpenEditPost"
+export { RemovePostIcon } from "../../remove-post-icon/ui/RemovePostIcon"
