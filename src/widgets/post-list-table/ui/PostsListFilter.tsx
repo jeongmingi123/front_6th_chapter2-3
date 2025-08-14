12f11022c519ca696cd13eddfe2a1ecb764ed903
jeongmingi123 <60804074+jeongmingi123@users.noreply.github.com>
2025-08-14 19:19:15 +0900

    refactor : post list filter props 변경 및 단일책임 적용

---
+++ src/widgets/post-list-table/ui/PostsListFilter.tsx
@@ -0,0 +1,17 @@
+import { useAtom } from "jotai"
+import { sortByAtom } from "../../../store/postsAtoms"
+import { SelectTag } from "../../../features/filter/ui/SelectTag"
+import { SortBySelect } from "../../../features/filter/ui/SortBySelect"
+import { SortOrderSelect } from "../../../features/filter/ui/SortOrderSelect"
+import { PostSearch } from "../../../features/post/ui/PostSearch"
+
+export const PostsListFilter = () => {
+  return (
+    <div className="flex gap-4">
+      <PostSearch />
+      <SelectTag />
+      <SortBySelect />
+      <SortOrderSelect />
+    </div>
+  )
+}
