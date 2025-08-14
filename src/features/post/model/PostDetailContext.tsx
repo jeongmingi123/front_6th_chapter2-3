6f81f0bea9c1e285da8b0d123d7c40e3a8f13817
jeongmingi123 <60804074+jeongmingi123@users.noreply.github.com>
2025-08-15 01:12:24 +0900

    refactor : 주석 추가 및 fetchComments 제거

--- src/features/post/model/PostDetailContext.tsx
+++ src/features/post/model/PostDetailContext.tsx
@@ -5,11 +5,11 @@ import { Post, Comment } from "../../../types"
 export const selectedPostAtom = atom<Post | null>(null)
 export const showPostDetailDialogAtom = atom<boolean>(false)
 
-// 댓글 관련 상태 atoms
+// 댓글 관련 상태 atoms (TanStack Query로 대체되므로 제거 예정)
 export const commentsAtom = atom<Record<number, Comment[]>>({})
 export const searchQueryAtom = atom<string>("")
 
-// 댓글 관련 함수들을 생성하는 atoms
+// 댓글 관련 함수들을 생성하는 atoms (TanStack Query로 대체)
 export const addCommentAtom = atom(null, (_get, _set, _postId: number) => {
   // 댓글 추가 로직은 useComments 훅에서 처리
   // 여기서는 단순히 상태만 업데이트
@@ -27,12 +27,8 @@ export const likeCommentAtom = atom(null, (_get, _set, _params: { id: number; po
   // 댓글 좋아요 로직은 useComments 훅에서 처리
 })
 
-// 포스트 상세 보기 열기 함수를 생성하는 atom
-export const openPostDetailAtom = atom(
-  null,
-  (get, set, { post, fetchComments }: { post: Post; fetchComments: (postId: number) => void }) => {
-    set(selectedPostAtom, post)
-    fetchComments(post.id)
-    set(showPostDetailDialogAtom, true)
-  },
-)
+// 포스트 상세 보기 열기 함수를 생성하는 atom (fetchComments 제거)
+export const openPostDetailAtom = atom(null, (_get, set, { post }: { post: Post }) => {
+  set(selectedPostAtom, post)
+  set(showPostDetailDialogAtom, true)
+})
