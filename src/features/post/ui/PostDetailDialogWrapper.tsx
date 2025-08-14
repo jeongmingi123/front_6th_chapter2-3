2b795a77235a13419c689835b81e8ddaf2c1d87e
jeongmingi123 <60804074+jeongmingi123@users.noreply.github.com>
2025-08-15 01:02:19 +0900

    refactor(post-detail-dialog) : comments 및 searchQuery 변경

--- src/features/post/ui/PostDetailDialogWrapper.tsx
+++ src/features/post/ui/PostDetailDialogWrapper.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { useAtom } from "jotai"
-import { selectedPostAtom, showPostDetailDialogAtom, commentsAtom, searchQueryAtom } from "../model/PostDetailContext"
-import { useComments } from "../../../features/comment/hooks/useComments"
+import { selectedPostAtom, showPostDetailDialogAtom } from "../model/PostDetailContext"
+import { useComments } from "../../comment"
 import { PostDetailDialog } from "../../../widgets"
 import { CommentsSection } from "../../../widgets/comment/ui/CommentsSection"
 import { Comment } from "../../../types"
@@ -9,10 +9,9 @@ import { Comment } from "../../../types"
 export const PostDetailDialogWrapper: React.FC = () => {
   const [selectedPost] = useAtom(selectedPostAtom)
   const [showPostDetailDialog, setShowPostDetailDialog] = useAtom(showPostDetailDialogAtom)
-  const [comments] = useAtom(commentsAtom)
-  const [searchQuery] = useAtom(searchQueryAtom)
 
-  const { addComment, updateComment, deleteComment, likeComment } = useComments()
+  // selectedPost가 있을 때만 useComments 훅 사용
+  const { addComment, updateComment, deleteComment, likeComment, comments } = useComments(selectedPost?.id)
 
   // 댓글 추가 다이얼로그 열기
   const handleOpenAddComment = (postId: number) => {
@@ -32,8 +31,8 @@ export const PostDetailDialogWrapper: React.FC = () => {
   }
 
   // 댓글 좋아요 처리
-  const handleLikeComment = (id: number, postId: number) => {
-    likeComment(id, postId)
+  const handleLikeComment = (id: number) => {
+    likeComment(id)
   }
 
   return (
@@ -41,13 +40,13 @@ export const PostDetailDialogWrapper: React.FC = () => {
       open={showPostDetailDialog}
       onOpenChange={setShowPostDetailDialog}
       selectedPost={selectedPost}
-      searchQuery={searchQuery}
+      searchQuery=""
     >
       {selectedPost && (
         <CommentsSection
           postId={selectedPost.id}
-          comments={comments[selectedPost.id] || []}
-          searchQuery={searchQuery}
+          comments={comments}
+          searchQuery=""
           onAddComment={handleOpenAddComment}
           onEditComment={handleOpenEditComment}
           onDeleteComment={handleDeleteComment}
